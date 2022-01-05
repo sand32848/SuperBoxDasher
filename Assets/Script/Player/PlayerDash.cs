0a094dbee8dc0a7f26c3ef851937597d18e4115a
sand32848 <54356212+sand32848@users.noreply.github.com>
2022-01-05 21:04:51 +0700

    Update CameraShake

--- Assets/Script/Player/PlayerDash.cs
+++ Assets/Script/Player/PlayerDash.cs
@@ -9,17 +9,18 @@ using DG.Tweening;
 public class PlayerDash : MonoBehaviour
 {
 	[SerializeField] private float DashTime;
-
+	[SerializeField] private LayerMask layerMask;
 	private PlayerAction playerInputActions;
 	private Camera mainCam;
 	private InputController inputController;
 	private bool isAiming = false;
-	private bool isCharging = false;
+	private LineRenderer lineRenderer;
 
 	private void Start()
     {
 		mainCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
 		inputController = GetComponent<InputController>();
+		lineRenderer = GetComponent<LineRenderer>();
 	}
 
 	private void Update()
@@ -30,17 +31,31 @@ public class PlayerDash : MonoBehaviour
     public void Dash()
 	{
 		Vector2 mousePosition = mainCam.ScreenToWorldPoint(inputController.mousePos);
-		
+
+		Vector2 aimPos = (mousePosition - (Vector2)transform.position).normalized;
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, aimPos, 50000, layerMask);
+
 		if (inputController.LeftHold)
 		{
-			isCharging = true;
-			isAiming = true; 
+			isAiming = true;
+			lineRenderer.enabled = true;
+		
+			
+			if(hit.collider != null)
+			{
+				lineRenderer.SetPosition(0, transform.position);
+				lineRenderer.SetPosition(1, hit.point);
+			}
 		}
 
 		if(!inputController.LeftHold && isAiming)
 		{
+			lineRenderer.enabled = false;
 			isAiming = false;
-			transform.DOMove(mousePosition,DashTime).SetEase(Ease.OutExpo); 
+			transform.DOMove(hit.point,DashTime).SetEase(Ease.OutExpo);
+			//mousePosition
+			StartCoroutine(mainCam.transform.Shake(0.2f, 0.5f));
 		}
 
     }
---
+++ Assets/Script/Utilities/HelperScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public static class HelperScript
+{
+	public static IEnumerator Shake(this Transform transform, float Duration = 0, float Power = 0)
+	{
+		Vector3 originalPos = transform.localPosition;
+
+		float elapsed = 0.0f;
+
+		while (elapsed < Duration)
+		{
+			float x = Random.Range(-1f, 1f) * Power;
+			float y = Random.Range(-1f, 1f) * Power;
+
+			transform.DOMove(new Vector3(x, y, originalPos.z), 0.1f).OnComplete(() => {
+				transform.DOMove(originalPos, 0.1f);
+				}); 
+
+		elapsed += Time.deltaTime;
+
+		yield return null;
+		}
+
+		transform.localPosition = originalPos;
+	}
+}
